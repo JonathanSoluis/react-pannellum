42a4e884e89aec5fb0c9ce60d89c59663783c6b1
Born Creative <hi@born-creative.co.uk>
2021-10-02 18:58:35 +0100

    mods

--- src/libs/pannellum.js
+++ src/libs/pannellum.js
@@ -612,8 +612,7 @@ export default (function (window, document, undefined) {
 
         // Load panorama
         console.log('load panorama image', image)
-        panoImage.src = server_url;
-        panoImage.crossOrigin = "anonymous"
+        panoImage.src = await window.URL.createObjectURL(image);
         console.log('panoImage.src', panoImage.src);
       });
       reader.addEventListener("error", function () {
